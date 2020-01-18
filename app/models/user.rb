334f988f9eeabd5df06a63ec48f500dd24ca0acc
Edemone <edem.agbenyo@gmail.com>
2020-01-18 13:02:27 +0000

    Implement user model test for validation and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts, -> { order "created_at DESC" }, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, -> { order "created_at DESC" }, dependent: :destroy
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:posts) { Post.new(body: "post content") }
+  subject {described_class.new(email: "edem@gmail.com", password: "secret")}
+  describe "Validations" do 
+    it "is valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "has minimum email of 5" do
+      subject.email = "a@g"
+
+      expect(subject).to be_valid
+    end
+  end
+
+  describe "Associations " do
+    
+    it "has one or more posts" do
+      assoc = described_class.reflect_on_association(:posts)
+      post1 = Post.create(body: "test 1 is here", user:subject)
+      post2 = Post.create(body: "test 1 is here", user:subject)
+      
+      expect(subject.posts.count).to eq 2
+    end
+  end
 end
