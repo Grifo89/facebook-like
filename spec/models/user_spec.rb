fea2992ee77f1d8fdb90d4101c9f7a2569b83adb
Edemone <edem.agbenyo@gmail.com>
2020-01-21 21:29:45 +0000

    Implement friendship in user model with user/friendship integration test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:posts) { Post.new(body: 'post content') }
+  let(:friendships){Friendship.new}
+  let(:inverse_friendships){Friendship.new}
   subject { described_class.new(email: 'edem@gmail.com', password: 'secret') }
   describe 'Validations' do
     it 'is valid with valid attributes' do
@@ -34,6 +36,16 @@ RSpec.describe User, type: :model do
       Post.create(body: 'test 1 is here', user: subject)
 
       expect(subject.posts.count).to eq 2
+    end
+
+    it "has one or many friends" do
+      assoc = described_class.reflect_on_association(:friendships)
+      expect(assoc.macro).to eq :has_many
+    end
+
+    it "has one or many inverse friends" do
+      assoc = described_class.reflect_on_association(:inverse_friendships)
+      expect(assoc.macro).to eq :has_many
     end
   end
 end
