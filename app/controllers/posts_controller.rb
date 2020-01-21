94b0750428ebfc39db4b6ac5b0d973f44dbc6483
Edemone <edem.agbenyo@gmail.com>
2020-01-21 15:54:50 +0000

    Code review issues fixes: show users pages posts controller to manage posts on timeline add post author to the list of post on timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,12 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @user = User.find(current_user.id)
+    @post = Post.new
+
+    @user_posts = current_user.posts
+  end
   def create
     @post = current_user.posts.new(body: post_params[:body])
     if @post.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'profile#index'
-  get 'profile', to: 'profile#index'
+  root 'posts#index'
+  get 'posts', to: 'posts#index'
   devise_for :users
   resources :posts
+  get 'users/index', to: 'users#index'
 end
