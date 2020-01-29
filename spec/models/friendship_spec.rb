98788ca399606663018cc6b3c6f5d2790c28b77f
Christian Salazar <calaca89@gmail.com>
2020-01-29 13:44:05 -0500

    Fix linter errors

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  let(:user1) { User.create(email: "userA@email.com", password: "secret")}
-  let(:user2) { User.create(email: "userB@email.com", password: "secret")}
-  describe "When a user sends a friend request" do
+  let(:user1) { User.create(email: 'userA@email.com', password: 'secret') }
+  let(:user2) { User.create(email: 'userB@email.com', password: 'secret') }
+  describe 'When a user sends a friend request' do
     it "The receiver isn't allowed to send a friend request to the same sender" do
       user1.friendships.create(receiver_id: user2.id)
       inverse_friendships = user2.friendships.new(receiver_id: user1.id)
