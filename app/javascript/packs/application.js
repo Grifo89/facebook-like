b4fee5960c721703d403ee651fe7d1ef8fec9de7
Edem Agbenyo <gedem4r@gmail.com>
2020-01-29 21:23:22 +0000

    Revert "Milestone8"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("bootstrap/dist/js/bootstrap")
-// require("jquery")
 
-import "bootstrap"
-import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,7 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'receiver_id'
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-    end
-  end
+         :recoverable, :rememberable, :validatable
 
   def friends
     friends_array = friendships.map { |friendship| friendship.receiver if friendship.status }
