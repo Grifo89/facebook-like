dcf72fecde185a7c5325727f3f653d975f16d8ad
Christian Salazar <calaca89@gmail.com>
2020-01-29 11:07:44 -0500

    Add bootstrap jquery and popper.js trought yarn and set them up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("bootstrap/dist/js/bootstrap")
-// require("jquery")
+require("bootstrap/dist/js/bootstrap")
+require("jquery")
 
-import "bootstrap"
-import "../stylesheets/application"
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
