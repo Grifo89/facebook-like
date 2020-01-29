86203383ea092d48b232dbf4bf80dddaab9639ee
Christian Salazar <calaca89@gmail.com>
2020-01-29 12:08:37 -0500

    Try to setup bootstrap4

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 require("bootstrap/dist/js/bootstrap")
 require("jquery")
 
-import "bootstrap"
-import "../stylesheets/application"
+import 'bootstrap'
+import './src/application.scss'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
