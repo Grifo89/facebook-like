e66fa14dbf86026cf1c15dc70e87c6cc1315452e
Edemone <edem.agbenyo@gmail.com>
2020-02-01 01:02:20 +0000

    Fix linter errors

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -11,9 +11,9 @@ class Friendship < ApplicationRecord
     puts " userid: #{user_id} receiver_id: #{receiver_id}"
     if Friendship.where(user_id: user.id, receiver_id: receiver.id, status: true).any? || Friendship.where(user_id: receiver.id, receiver_id: user.id, status: true).any?
       errors.add(:user_id, 'Already friends!')
-      return false
+      false
     else
-      return true
+      true
     end
   end
 end
